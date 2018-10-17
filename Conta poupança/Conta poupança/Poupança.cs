bdf77edb998c24f416ba4597f005990b9632dfd6
LemuelB <36803456+LemuelB@users.noreply.github.com>
2018-10-16 21:52:51 -0300

    Add files via upload

---
+++ Conta poupança/Conta poupança/Poupança.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_poupança
+{
+    class Poupança
+    {
+        private decimal saldoPoupanca;
+        private static double taxaDeJurosAnual;
+
+        private const int MESES = 12;
+
+        
+        public Poupança()
+        {
+            saldoPoupanca = 0.0M;
+            taxaDeJurosAnual = 0.0;
+        }
+
+       
+        public Poupança(decimal saldoPoupanca)
+        {
+            this.saldoPoupanca = saldoPoupanca;
+            taxaDeJurosAnual = 0.0;
+        }
+
+        
+        public decimal Saldo
+        {
+            get { return saldoPoupanca; }
+            set { saldoPoupanca = value; }
+        }
+
+        
+        public static double TaxaJuros
+        {
+            get { return taxaDeJurosAnual; }
+            set { taxaDeJurosAnual = value; }
+        }
+
+        
+        public decimal CalcularJurosMensal()
+        {
+            decimal jurosMensal = (saldoPoupanca * (decimal)taxaDeJurosAnual) / MESES;
+            return jurosMensal + saldoPoupanca;
+        }
+
+        
+        public static void AlterarTaxaDeJuros(double taxaDeJuros)
+        {
+            taxaDeJurosAnual = taxaDeJuros;
+        }
+    }
+}
---
+++ Conta poupança/Conta poupança/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_poupança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Poupança poupador1 = new Poupança(2000M);
+                Poupança poupador2 = new Poupança(3000M);
+                decimal saldoPoupador1 = 0.0M;
+                decimal saldoPoupador2 = 0.0M;
+
+
+                Console.WriteLine("Taxa de Juros = 4%");
+                Poupança.AlterarTaxaDeJuros(0.04);
+
+
+                saldoPoupador1 = poupador1.CalcularJurosMensal();
+                saldoPoupador2 = poupador2.CalcularJurosMensal();
+                Console.WriteLine("   Poupador 1 = {0:C}", saldoPoupador1);
+                Console.WriteLine("   Poupador 2 = {0:C}", saldoPoupador2);
+
+
+                Console.WriteLine("\nTaxa de Juros = 5%");
+                Poupança.AlterarTaxaDeJuros(0.05);
+
+                saldoPoupador1 = poupador1.CalcularJurosMensal();
+                saldoPoupador2 = poupador2.CalcularJurosMensal();
+                Console.WriteLine("   Poupador 1 = {0:C}", saldoPoupador1);
+                Console.WriteLine("   Poupador 2 = {0:C}", saldoPoupador2);
+
+
+                Console.Write("\n\nPressione qualquer tecla para sair . . .");
+                Console.ReadKey(true);
+
+            }
+        }
+    }
+}
